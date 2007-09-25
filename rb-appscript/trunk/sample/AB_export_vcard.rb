5ef6af53f697be9d8575ac967ad96d455ac937b4
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2007-09-25 00:07:56 +0000

    [rb-appscript] improved AB_export_vcards.rb sample
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@312 5e899d65-cff2-4c64-b955-3fa930e4d869

--- rb-appscript/trunk/sample/AB_export_vcard.rb
+++ rb-appscript/trunk/sample/AB_export_vcard.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env ruby
 
 # Exports all Address Book entries as vcards to current working folder.
-# Files are named as 'FIRSTNAME LASTNAME.vcard'; any missing
-# names are replaced with '???'.
 #
-# (Note: if two people have identical names, the first vcard file will be
-# overwritten by the second. The script could be modified to handle
-# this condition if necessary.)
+# Files are named as 'NAME.vcard' (note: existing files will be overwritten).
+# If the name is missing, 'unknown' is used instead. If two or more people
+# share the same name, files will be named 'NAME.vcard', 'NAME 1.vcard',
+# 'NAME 2.vcard', etc.
 
 
 # Note: if using the appscript gem, rubygems must be required first:
@@ -17,8 +16,16 @@ include Appscript
 
 people = app('Address Book').people
 
-people.first_name.get.zip(people.last_name.get, people.vcard.get).each do |first, last, vcard|
-	first = '???' if first == :missing_value
-	last = '???' if last == :missing_value
-	File.open("#{first} #{last}.vcard".gsub('/', ':'), 'w') { |f| f.puts vcard }
+found_names = []
+people.name.get.zip(people.vcard.get).each do |name, vcard|
+	name = 'unknown' if name == ''
+	name = name.gsub('/', ':')
+	filename = "#{name}.vcard"
+	i = 1
+	while found_names.include?(filename.downcase)
+		filename = "#{name} #{i}.vcard"
+		i += 1
+	end
+	found_names.push(filename.downcase)
+	File.open(filename, 'w') { |f| f.puts vcard }
 end
