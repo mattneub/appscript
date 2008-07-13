dbf337f90e9555281e188c4466519f5da6adcf42
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2008-07-13 12:32:40 +0000

    [py-aeserver] tagged 0.2.0
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@545 5e899d65-cff2-4c64-b955-3fa930e4d869

---
+++ py-aeserver/tags/py-aeserver-0.2.0/lib/aemreceive/handlererror.py
@@ -0,0 +1,29 @@
+"""handlererror -- Used to pass known errors back to client.
+
+(C) 2005 HAS
+"""
+
+from aem import kae
+
+
+class EventHandlerError(Exception):
+	"""Event-handling callbacks should raise an EventHandlerError exception to send an error message back to client."""
+	def __init__(self, number, message=None, object=None, coercion=None):
+		self.number = number
+		self.message = message
+		self.object = object
+		self.coercion = coercion
+		Exception.__init__(self, number, message, object, coercion)
+	
+	def get(self): # used internally by aemreceive
+		result = {kae.keyErrorNumber: self.number}
+		for key, val in [
+				(kae.keyErrorString, self.message), 
+				(kae.kOSAErrorOffendingObject, self.object), 
+				(kae.kOSAErrorExpectedType, self.coercion)]:
+			if val is not None:
+				result[key] = val
+		return result
+	
+	def __str__(self):
+		return '%s (%i)' % (self.message, self.number)
---
+++ py-aeserver/tags/py-aeserver-0.2.0/setup.py
@@ -0,0 +1,22 @@
+try:
+	from setuptools import setup, Extension
+except ImportError:
+	print "Note: couldn't import setuptools so using distutils instead."
+	from distutils.core import setup, Extension
+
+
+setup(
+		name = "aeserver",
+		version = "0.2.0",
+		description = "Apple event handling framework",
+		author = "HAS",
+		author_email='',
+		url='http://appscript.sourceforge.net',
+		license='MIT',
+		platforms=['Mac OS X'],
+		packages = [
+			'aemreceive',
+		],
+		extra_path = "aeosa",
+		package_dir = { '': 'lib' }
+)
