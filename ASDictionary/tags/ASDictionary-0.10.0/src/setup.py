9e6ab427b81de79a7f649bf80fc3224831b1b2eb
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2008-07-13 12:30:10 +0000

    [ASDictionary] tagged 0.10.0
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@542 5e899d65-cff2-4c64-b955-3fa930e4d869

---
+++ ASDictionary/tags/ASDictionary-0.10.0/src/setup.py
@@ -0,0 +1,61 @@
+"""ASDictionary build notes
+
+Requirements:
+
+- Python 2.4+ <http://www.python.org>
+
+- py-appscript 0.19.0+ <http://appscript.sourceforge.net>
+- py-aeserver 0.2.0+
+- py-osaterminology 0.12.0+
+
+- PyObjC <http://pyobjc.sourceforge.net/>
+
+- py2app <http://undefined.org/python/>
+
+--
+
+To build, cd to ASDictionary/src directory and run:
+
+	python setup.py py2app
+
+"""
+
+from distutils.core import setup
+import py2app
+from plistlib import Plist
+import os
+
+name = 'ASDictionary'
+version='0.10.0'
+
+os.system('''
+sdp -fa %s.sdef;
+Rez %sScripting.r -o %s.rsrc -useDF
+''' % ((name,) * 3))
+
+setup(
+	app=[name+".py"],
+	data_files=["MainMenu.nib"],
+	options=dict(
+	
+
+		py2app=dict(
+			plist=Plist(
+				NSAppleScriptEnabled=True,
+				CFBundleVersion=version,
+				CFBundleShortVersionString=version,
+				NSHumanReadableCopyright="(C) 2005-2008 HAS",
+				CFBundleIdentifier="net.sourceforge.appscript.asdictionary",
+				CFBundleDocumentTypes = [
+					dict(
+						CFBundleTypeExtensions=["*"],
+						CFBundleTypeName="public.item",
+						CFBundleTypeRole="Viewer",
+					),
+				]
+			),
+			resources=[name+'.icns', name+'.rsrc'],
+			iconfile=name+'.icns'
+		)
+	)
+)
