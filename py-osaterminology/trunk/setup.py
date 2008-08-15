551d8249226873fe47d398705ac19dc638552855
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2008-08-15 07:05:36 +0000

    [py-osaterminology] updated version to 0.13.0
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@563 5e899d65-cff2-4c64-b955-3fa930e4d869

--- py-osaterminology/trunk/setup.py
+++ py-osaterminology/trunk/setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
 		name = "osaterminology",
-		version = "0.12.1",
+		version = "0.13.0",
 		description = "Parse and render aete/sdef resources.",
 		author = "HAS",
 		author_email='',
