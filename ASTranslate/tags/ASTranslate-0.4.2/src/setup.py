fb612a2c4fa0251fed797bda5c44268c10b23f61
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2008-08-15 07:14:38 +0000

    [ASTranslate] tagged 0.4.2
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@567 5e899d65-cff2-4c64-b955-3fa930e4d869

---
+++ ASTranslate/tags/ASTranslate-0.4.2/src/setup.py
@@ -0,0 +1,55 @@
+"""
+Requirements:
+
+- py-appscript 0.19.0+ <http://appscript.sourceforge.net>
+
+- py-osascript 0.5.0+
+
+- py-osaterminology 0.11.0+
+
+- pyobjc <http://pyobjc.sourceforge.net>
+
+- py2app <http://undefined.org/python/>
+
+--
+
+To build, cd to this directory and run:
+
+	python setup.py py2app
+
+"""
+
+from distutils.core import setup, Extension
+import py2app
+from plistlib import Plist
+
+version = '0.4.2'
+
+
+setup(
+	app=["ASTranslate.py"],
+	data_files=["MainMenu.nib", "ASTranslateDocument.nib"],
+	options=dict(
+	
+		py2app=dict(
+			plist=Plist(
+				NSAppleScriptEnabled=True,
+				CFBundleIdentifier="net.sourceforge.appscript.astranslate",
+				CFBundleVersion=version,
+				CFBundleShortVersionString=version,
+				NSHumanReadableCopyright="(C) 2007-2008 HAS",
+				CFBundleDocumentTypes = [
+					dict(
+						CFBundleTypeExtensions=[],
+						CFBundleTypeName="Text File",
+						CFBundleTypeRole="Editor",
+						NSDocumentClass="ASTranslateDocument"
+					)
+				]
+			),
+			resources=['ASTranslate.icns'],
+			iconfile='ASTranslate.icns'
+		)
+	)
+)
+
