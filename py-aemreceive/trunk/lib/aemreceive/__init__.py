ca255c856fa18d87b6df8d876d1746a794806d64
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2008-11-14 19:31:18 +0000

    [py-aemreceive] updated kAE to kae
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@623 5e899d65-cff2-4c64-b955-3fa930e4d869

--- py-aemreceive/trunk/lib/aemreceive/__init__.py
+++ py-aemreceive/trunk/lib/aemreceive/__init__.py
@@ -1,11 +1,11 @@
 """aemreceive -- Install Python functions as Apple event handlers.
 
-(C) 2005 HAS
+(C) 2005-2008 HAS
 """
 
 from aem import AEType, AEEnum # re-exported for convenience
 from aem import kae # re-export for convenience
-kAE = kae # for backwards compatibility; TO DO: deprecated, delete
+kAE = kae # backwards compatibility; deprecated
 
 from main import kMissingValue, Codecs, installeventhandler, removeeventhandler, installcoercionhandler, removecoercionhandler
 from handlererror import EventHandlerError
