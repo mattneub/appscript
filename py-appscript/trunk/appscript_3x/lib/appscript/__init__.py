0847decd68549d5886646624452ef631f9cab8e6
hhas <hhas@5e899d65-cff2-4c64-b955-3fa930e4d869>
2009-11-18 17:38:12 +0000

    [py-appscript] fixed faulty code added to py3-osax in previous revision; improved notes on 64-bit support in osax module documentation; updated version number to 0.20.2
    git-svn-id: https://appscript.svn.sourceforge.net/svnroot/appscript@690 5e899d65-cff2-4c64-b955-3fa930e4d869

--- py-appscript/trunk/appscript_3x/lib/appscript/__init__.py
+++ py-appscript/trunk/appscript_3x/lib/appscript/__init__.py
@@ -3,7 +3,7 @@
 (C) 2004-2008 HAS
 """
 
-__version__ = '0.20.1'
+__version__ = '0.20.2'
 
 __all__ = ['ApplicationNotFoundError', 'CommandError', 'CantLaunchApplicationError', 
 		'app','con', 'its', 'k','mactypes']
--- py-appscript/trunk/setup.py
+++ py-appscript/trunk/setup.py
@@ -16,7 +16,7 @@ else:
 
 setup(
 		name = "appscript",
-		version = "0.20.1",
+		version = "0.20.2",
 		description = "Control scriptable Mac OS X applications and scripting additions from Python.",
 		author = "HAS",
 		author_email='',
